0f2c895c5253a75b9c33cc7ea63ceb25aaa176da
Krisan Alfa Timur <krisan47@gmail.com>
2018-03-20 12:37:46 +0800

    [FIX] Marking changes.

--- src/controllers/SiteController.ts
+++ src/controllers/SiteController.ts
@@ -4,9 +4,10 @@ import { random } from './../libs/helpers'
 export default class SiteController {
   public async index (context: Context) {
     context.body = {
+      version: '0.0.1',
       message: 'success',
       data: {
-        '$hashes': [...(function* () { let i = 0; while (i < 8) yield i++ })()].map((): string => random())
+        '$hashes': [...(function* () { let i = 0; while (i <= 10) yield i++ })()].map((): string => random()),
       }
     }
   }
