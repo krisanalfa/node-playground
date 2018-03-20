0fc787edde6a6325ebb5fdc736f076dad85df1f7
Krisan Alfa Timur <krisan47@gmail.com>
2018-03-20 12:40:33 +0800

    [BUMP] Version 0.0.2.

--- src/controllers/SiteController.ts
+++ src/controllers/SiteController.ts
@@ -4,7 +4,7 @@ import { random } from './../libs/helpers'
 export default class SiteController {
   public async index (context: Context) {
     context.body = {
-      version: '0.0.1',
+      version: '0.0.2',
       message: 'success',
       data: {
         '$hashes': [...(function* () { let i = 0; while (i <= 10) yield i++ })()].map((): string => random())
